4aee010efab5b6fd9372c001d1d592e749c1bf15
Unknown <martacypel@gmail.com>
2018-02-27 22:37:16 +0000

    menu actualization when the scroll is down

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+$(document).ready(
+    function()
+    {
+        $(window).scroll(function(){
+                if($(window).scrollTop() > 480){
+                    $("nav").addClass("navbarScroll");
+                }
+                else{
+                    $("nav").removeClass("navbarScroll");
+                }
+            }
+        );
+    }
+);
+
+
+
+
 function myMap() {
     var mapOptions = {
         center: new google.maps.LatLng(40.8387294, -8.4769763),
